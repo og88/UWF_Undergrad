a4732971bfec71971aa4e90615574c22f3eaeae8
Omar Garcia <og8@students.uwf.edu>
2018-01-24 13:04:21 -0600

    Orginize projects into more specific folder. This makes finding them easier.

--- Fall 2007/nerdy/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-javafxc src/Game/Main.fx
--- Fall 2007/nerdy/dist.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-javafxpackager -src ./src -appClass Game.Main -pack200 -appName Nerdy -appVendor Silveira -appVersion 1.0 -appHeight 384 -appWidth 416 -draggable
