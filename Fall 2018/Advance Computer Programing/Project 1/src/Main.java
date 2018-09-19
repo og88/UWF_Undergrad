e85be4a66f427250cd5d41cef194e12e77d4274f
Omar Garcia <og8@students.uwf.edu>
2018-09-19 12:15:20 -0500

    add reflection to retrieve information to create a SQL Table.

--- Fall 2018/Advance Computer Programing/Project 1/src/Main.java
+++ Fall 2018/Advance Computer Programing/Project 1/src/Main.java
@@ -1,15 +1,48 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Vehicle v1 = new Vehicle();
-        System.out.println(v1.getMake());
-        v1.setMake("Ford");
-        System.out.println(v1.getMake());
+
+        Class vehicle = Vehicle.class; //Creates a class object for our reflection processes
+
+        createTable(vehicle);
+
+    }
+
+    /**
+     * This method auto generates a SQL table from a jav class.
+     * It uses the class name for the table name.
+     * Each of the classes variables are the row names.
+     * The variable types are the item types as well.
+     * @param class1 The method extracts all needed information form this class.
+     */
+    static void createTable(Class class1)
+    {
         Writer write1 = new Writer("dbOperations.log");
         write1.startNew("DB Log \n");
-        write1.add("Test");
+        write1.add("Test\n");
+
+        String className = class1.getName();
+
+        Field[] fields = class1.getDeclaredFields();
+
+        String createTable = "CREATE TABLE " + className + "(\n";
+        write1.add("Table " + className + " Created.\n\n");
+        for(Field field: fields)
+        {
+            if(field.getType().getName() == "java.lang.String")
+            {
+                createTable = createTable + field.getName() + " String,\n";
+            }
+            else {
+                createTable = createTable + field.getName() + " " + field.getType().getName() + ",\n";
+            }
+        }
+        createTable = createTable + ");";
+
+        System.out.print(createTable);
 
     }
 }
